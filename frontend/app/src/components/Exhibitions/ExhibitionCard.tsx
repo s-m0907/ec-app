78ff84ef139eb9f739583a3ba1566ed91397374e
s-m0907 <suzewhaites@gmail.com>
2024-10-12 13:55:21 +0100

    image fetch logic and exhibition card ui

--- frontend/app/src/components/Exhibitions/ExhibitionCard.tsx
+++ frontend/app/src/components/Exhibitions/ExhibitionCard.tsx
@@ -1,14 +1,84 @@
 import { Exhibition } from "../../firebase"
+import styled from "styled-components"
+import CardImage from "./CardImage"
+import useFetch from "../../hooks/useFetch"
+import { useEffect, useState } from "react"
+
+const Container = styled.div`
+flex: 1 1 calc(25% - 16px);
+padding: 16px;
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+border: solid black 2px;
+
+@media(max-width: 1200px) {
+    flex: 1 1 calc(33.33% - 16px);
+}
+ 
+@media (max-width: 800px) {
+    flex: 1 1 calc(50% - 16px);
+}
+
+@media(max-width: 480px) {
+    flex: 1 1 100%;
+}
+`
+
+const ImageGrid = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr;
+grid-template-rows: 1fr 1fr;
+gap: 10px;
+width: 100%;
+height: 500px;
+`
+
+const Title = styled.h3`
+font-weight: 500;
+`
+
+const Footer = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+background-color: whitesmoke;
+padding: 0 20px;
+margin: 20px -16px -16px;`
 
 interface ExhibitionCardProps {
     exhibition: Exhibition
 }
 
-
 const ExhibitionCard: React.FC<ExhibitionCardProps> = ({exhibition}) => {
+    const idString = exhibition.artwork_ids.join(',')
+    const {data, error} = useFetch<any>(`https://api.artic.edu/api/v1/artworks?ids=${idString}0&fields=id,title,image_id`)
+    const [imageIds, setImageIds] = useState<string[]>([])
 
-    return <>{exhibition.exhibition_name}</>
+    useEffect(() => {
+        if (data && data.data) {
+            const idArray = data.data.map((item: any) => item.image_id)
+            setImageIds(idArray.slice(0,3))
+            } else if (error) {
+                console.error(error)
+            }
+    }, [data])
 
+    if(imageIds.length !== 0) {
+    return <Container>
+        <ImageGrid>
+        {imageIds.map((imageId, index) => {
+            return <CardImage imageId={imageId} url={data.config.iiif_url} index={index}/>
+        })}
+        </ImageGrid>
+        <Footer>
+        <Title>{exhibition.exhibition_name}</Title>
+        <p>{data.data.length} artworks</p>
+        </Footer>
+        </Container>
+    }
+return <></>
 }
 
 export default ExhibitionCard
--- frontend/app/src/components/Exhibitions/ExhibitionsList.tsx
+++ frontend/app/src/components/Exhibitions/ExhibitionsList.tsx
@@ -2,6 +2,18 @@ import { useEffect, useState } from "react"
 import { Exhibition, getExhibitions } from "../../firebase"
 import { useAuth } from "../../contexts/Auth"
 import ExhibitionCard from "./ExhibitionCard"
+import styled from "styled-components"
+
+const Grid = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+gap: 16px;
+padding: 16px
+`
+const Info = styled.h3`
+margin: 10px 20px 0;
+text-align: right;`
 
 const ExhibitionsList: React.FC = () => {
     const [exhibitions, setExhibitions] = useState<Exhibition[]>([])
@@ -13,7 +25,6 @@ const ExhibitionsList: React.FC = () => {
                 try {
                     const data = await getExhibitions(user.uid)
                     setExhibitions(data || [])
-                    console.log(exhibitions)
                 } catch (error: any) {
                     console.error("Error fetching exhibitions", error)
                 }
@@ -23,9 +34,12 @@ const ExhibitionsList: React.FC = () => {
     },[])
 
     return <>
+    <Info>{exhibitions.length} Exhibitions</Info>
+    <Grid>
     {exhibitions.map((exhibition) => {
-        return <ExhibitionCard exhibition = {exhibition}/>
+        return <ExhibitionCard key={exhibition.id} exhibition = {exhibition}/>
     })}
+    </Grid>
     </>
 }
 
